ce83bb684756ccb885d994fd773f8cf16ba76fb0
Zatomis <Zatomis@list.ru>
2023-05-07 11:18:17 +0000

    Изменен код

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ import argparse
 from dotenv import load_dotenv, find_dotenv
 
 
-def is_active_link(url):
-  response = requests.get(url)
-  response.raise_for_status()
-  return response.ok
-
-
 def is_bitlink(token, url):
-  bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url.strip()}"
+  bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
   headers = {'Authorization': f'Bearer {token}'}
   response = requests.get(bitly_url, headers=headers)
   response.raise_for_status()
@@ -22,13 +16,13 @@ def is_bitlink(token, url):
 
 def shorten_link(token, url):
   bitly_url = "https://api-ssl.bitly.com/v4/bitlinks"
-  params = {'long_url': f'{url}'}
+  params = {'long_url': url}
   headers = {'Authorization': f'Bearer {token}'}
   response = requests.post(bitly_url, headers=headers, json=params)
   if (response.ok):
-    return json.loads(response.text)["id"]
+    return (response.json)["id"]
   else:
-    return "OtherShotLink"
+    return False
 
 
 def click_count(token, url):
@@ -36,7 +30,7 @@ def click_count(token, url):
   headers = {'Authorization': f'Bearer {token}'}
   response = requests.get(bitly_url, headers=headers)
   response.raise_for_status()
-  return json.loads(response.text)["total_clicks"]
+  return (response.json)["total_clicks"]
 
 
 def createParser ():
@@ -45,31 +39,23 @@ def createParser ():
     return parser
 
 
-def main(link_convert):
-  load_dotenv(find_dotenv())
-  token_key = os.environ.get("BITLINK_TOKEN")
-
-  if (link_convert == "None"):
-    bitlink = input("Введите ссылку : ")
-  else:
-    bitlink = link_convert
-    
-  try:
-    if is_bitlink(token_key, bitlink):
-        print("Кол-во переходов по ссылке : " +
-              str(click_count(token_key, bitlink)))
-    else:
-      if is_active_link(bitlink):
-        shot_link = shorten_link(token_key, bitlink)
-        if (shot_link != "OtherShotLink"):
-          print("Короткая ссылка : " + shot_link + "\nПереходов по ней : " +
-                str(click_count(token_key, bitlink)))
-        else:
-          print("Это другая сокращенная ссылка : " + bitlink)
-  except requests.exceptions.HTTPError:
-    print("Ошибка в ссылке")
-
-
 if __name__ == '__main__':
   parser = createParser()
-  main(str(parser.parse_args(sys.argv[1:]).link))
+  link_convert = parser.parse_args(sys.argv[1:]).link
+  load_dotenv(find_dotenv())
+  token = os.environ.get("BITLINK_TOKEN", "ERROR")
+  if token=="ERROR":
+    print("Не указан токен Bitly")
+  else:  
+    if (str(link_convert) == "None"):
+      bitlink = input("Введите ссылку : ")
+    else:
+      bitlink = link_convert
+    try:
+      if is_bitlink(token, bitlink):
+          print(f"Кол-во переходов по ссылке : {str(click_count(token, bitlink))}")
+      else:
+          if (shorten_link(token, bitlink)):
+            print(f"Короткая ссылка : {shot_link}\nПереходов по ней : {str(click_count(token, bitlink))}")
+    except requests.exceptions.HTTPError:
+      print("Указана не верная ссылка")
